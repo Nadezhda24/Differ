196c84370beb4930ed8d274e611ad8b88e581392
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2023-04-01 19:17:11 +0300

    add compare

--- main.js
+++ main.js
@@ -5,5 +5,12 @@ const path2 = "testPages/1-src.html";
 const linerTreeList1 = parser(path1);
 const linerTreeList2 = parser(path2);
 
+let change = 0;
+linerTreeList2.forEach((item, index) => {
+   item.compare(linerTreeList1[index]);
+   if (item.status === "changed") change++;
+})
+
+console.log("change " + change);
 console.log(linerTreeList1.length);
 console.log(linerTreeList2.length);
--- model/LinerTree.js
+++ model/LinerTree.js
@@ -25,9 +25,15 @@ class LinerTree {
     }
 
     compare(linerTree) {
-        if (this._node !== linerTree.node) {
-            this._status = "changed";
-            linerTree.status = "changed";
+        if (!!this._node && !!linerTree && !!linerTree.node ) {
+            if (this._node.type !== "text" && this._node.name !== linerTree.node.name){
+                this._status = "changed";
+                linerTree.setStatus("changed");
+            }
+            if (this._node.type === "text" && this._node.data !== linerTree.node.data){
+                this._status = "changed";
+                linerTree.setStatus("changed");
+            }
         }
     }
 
